1024159dd01e465a1619f3bdeb999a9ed810627a
renas <55415770+ozi529@users.noreply.github.com>
2026-03-15 16:40:03 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T13:39:50.644Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T13:40:02.786Z
+Commit: Fix broken links in documentation
