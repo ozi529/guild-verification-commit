1cc2d162950a013d16c252d460ca0fc6b60761fb
renas <55415770+ozi529@users.noreply.github.com>
2026-03-15 16:39:25 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T13:39:19.697Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-15T13:39:24.271Z
+Commit: Resolve timezone issues in date handling
