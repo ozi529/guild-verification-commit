19d8142151f9834543af876b21be046774379f3e
renas <55415770+ozi529@users.noreply.github.com>
2026-03-15 16:39:40 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T13:39:25.638Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T13:39:39.421Z
+Commit: Fix broken links in documentation
