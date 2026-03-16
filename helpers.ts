4372af1eed755aedd1f9a193609b3711526ce3eb
renas <55415770+ozi529@users.noreply.github.com>
2026-03-16 21:30:27 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T18:30:13.997Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-16T18:30:26.442Z
+Commit: Fix database connection timeout
