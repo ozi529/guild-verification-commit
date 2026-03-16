70b0d85869ea7541fc1504f34901bb39f64a57f4
renas <55415770+ozi529@users.noreply.github.com>
2026-03-16 21:30:17 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T09:47:44.340Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-16T18:30:15.868Z
+Commit: Fix CSS styling bug on mobile devices
