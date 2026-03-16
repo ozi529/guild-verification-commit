a62d47391e7b759134ac2ec3ae45d58d4c39452d
renas <55415770+ozi529@users.noreply.github.com>
2026-03-16 12:47:14 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T13:40:18.112Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-16T09:47:12.848Z
+Commit: Resolve null pointer exception in user service
