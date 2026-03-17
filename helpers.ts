4e8e5ce2fbf6fc5511f1774105ed4d183bf9e2ad
renas <55415770+ozi529@users.noreply.github.com>
2026-03-17 12:49:04 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-17T09:49:00.441Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-17T09:49:03.726Z
+Commit: Fix CSS styling bug on mobile devices
